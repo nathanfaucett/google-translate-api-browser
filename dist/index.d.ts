6c2c272d3316b82ec36c8d82351a3329018150fc
Nathan Faucett <nfaucett@utility.com>
2023-06-02 09:02:57 -0400

    npm git version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,13 +2,5 @@ import { generateRequestUrl } from './generateRequestUrl';
 import { isSupported } from './languages';
 import { normaliseResponse } from './normaliseResponse';
 import { translate, setCORS } from './translate';
-
-export {
-  isSupported,
-  translate,
-  normaliseResponse,
-  generateRequestUrl,
-  setCORS,
-};
-
+export { isSupported, translate, normaliseResponse, generateRequestUrl, setCORS, };
 export default translate;
